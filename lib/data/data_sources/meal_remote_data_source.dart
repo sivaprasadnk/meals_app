c081e6299c86a4fb016d72dfc1310beeca55b1f8
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-06-23 13:45:17 +0530

    riverpod changes

--- lib/data/data_sources/meal_remote_data_source.dart
+++ lib/data/data_sources/meal_remote_data_source.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:meals_app/utils/string_constants.dart';
 
@@ -12,7 +11,6 @@ class MealRemoteDataSource {
   Future<Map<String, dynamic>> getRandomMeal() async {
     var url = baseUrl + randomMealEndPoint;
     final response = await client.get(Uri.parse(url));
-    // debugPrint('@@getRandomMeal response :${response.body}');
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
@@ -23,7 +21,6 @@ class MealRemoteDataSource {
   Future<Map<String, dynamic>> getMealCategories() async {
     var url = baseUrl + mealCategoriesEndPoint;
     final response = await client.get(Uri.parse(url));
-    debugPrint('@@getMealCategories response :${response.body}');
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
